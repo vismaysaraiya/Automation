738627aab10005b8908b42b5c04811866d6a3eda
vismay_qa <vismaysaraiya@gmail.com>
2024-01-23 19:15:26 +0530

    Hooks_Before

--- cypress/e2e/testcases.cy.js
+++ cypress/e2e/testcases.cy.js
@@ -1,34 +1,41 @@
+//const { before } = require("cypress/types/lodash")
 describe ('Testcases', function() {
-    it('Register User', function () {
-        
+    beforeEach( ()=>{
+        //Verify that home page is visible successfully
         cy.visit("http://automationexercise.com")
-
-       //Verify that home page is visible successfully
-        cy.findAllByText('Category').should('exist')
-        cy.findAllByText('Features Items').should('exist')
-        cy.findAllByText('Brands').should('exist')
        
-        //Click on 'Signup / Login' button
-        cy.get("a[href='/login']").click()
+        cy.findByText('Category').should('exist')
+        cy.findByText('Features Items').should('exist')
+        cy.findByText('Brands').should('exist')
 
+        //cy.get("a[href='/login']").click()
+
+    })
+    it('Register User', function () {
+       
         //Verify 'New User Signup!' is visible
         cy.findAllByText('Login to your account').should('exist')
         cy.findAllByText('New User Signup!').should('exist')
 
         // Enter name and email address
         cy.get('[data-qa=signup-name]').type('vismay')
-        cy.get('[data-qa=signup-email]').type('vs@yopmail.com')
+        cy.get('[data-qa=signup-email]').type('vsa@yopmail.com')
 
         //Click 'Signup' button
         cy.get('[data-qa=signup-button]').click()
        
         // Verify that 'ENTER ACCOUNT INFORMATION' is visible
+        //cy.get('.col-sm-4 col-sm-offset-1').contains('ENTER ACCOUNT INFORMATION')
+       
         cy.findAllByText('Enter Account Information').should('exist')
+        //cy.findAllByText('Title').should('exist')
+       
        
         //Fill details: Title, Name, Email, Password, Date of birth
         cy.findByLabelText('Mr.').check()
         cy.get('[id=name]').type('vismay')
         cy.get('[type=Password]').type('Crest@123')
+       
         cy.get('select[id=days]').select('18')
         cy.get('select[id=months]').select('November')
         cy.get('select[id=years]').select('1986')
@@ -72,17 +79,7 @@ describe ('Testcases', function() {
     }),
 
     it("Login User with correct email and password", ()=>{
-        // Navigate to url 'http:automationexercis.com'
-        cy.visit("https://automationexercise.com")
-
-        // Verify that home page is visible successfully
-        cy.findAllByText('Category').should('exist')
-        cy.findAllByText('Features Items').should('exist')
-        cy.findAllByText('Brands').should('exist')
-        
-        // Click on 'Signup / Login' button
-        cy.get("a[href='/login']").click()
-
+       
         // Verify 'Login to your account' is visible
         cy.findAllByText('Login to your account').should('exist')
         cy.findAllByText('New User Signup!').should('exist')
@@ -90,15 +87,90 @@ describe ('Testcases', function() {
         // Enter correct email address and password
         cy.get('[data-qa=login-email]').type('vismay@crest.com')
         cy.get('[type=Password]').type('Crest@123')
-        
+       
         // Click 'login' button
         cy.get('[data-qa=login-button]').click()
 
         // Verify that 'Logged in as username' is visible
         cy.get('.shop-menu').find('li').contains('Logged in as vismay')
 
+    }),
+       
+    it("Login User with incorrect email and password",()=>{
+       
+        // Enter incorrect email address and password
+        cy.get('[data-qa=login-email]').type('1vismay@crest.com')
+        cy.get('[type=Password]').type('Crest@1231')
+
+        // Click 'login' button
+        cy.get('[data-qa=login-button]').click()
+
+        // Verify error 'Your email or password is incorrect!' is visible
+        cy.findByText("Your email or password is incorrect!").should('exist')
+    //it("Login User with incorrect email and password",()=>)
+    }),
+
+    it(" Logout User",()=>{
+         // Enter correct email address and password
+         cy.get('[data-qa=login-email]').type('vismay@crest.com')
+         cy.get('[type=Password]').type('Crest@123')
+         
+         // Click 'login' button
+         cy.get('[data-qa=login-button]').click()
+
+         // Verify that 'Logged in as username' is visible
+         cy.get('.shop-menu').find('li').contains('Logged in as vismay')
+
+         //Click 'Logout' button
+         cy.get("a[href='/logout']").click()
+
+         //Verify that user is navigated to login page
+         cy.findAllByText('Login to your account').should('exist')
+         cy.findAllByText('New User Signup!').should('exist')
+
+    }),
+
+    it("Register User with existing email",()=>{
+        //Verify 'New User Signup!' is visible
+        cy.findAllByText('Login to your account').should('exist')
+        cy.findAllByText('New User Signup!').should('exist')
+
+        // Enter name and email address
+        cy.get('[data-qa=signup-name]').type('vismay')
+        cy.get('[data-qa=signup-email]').type('vismay@crest.com')
+
+        //Click 'Signup' button
+        cy.get('[data-qa=signup-button]').click()
+
+        //Verify error 'Email Address already exist!' is visible
+        cy.findByText("Email Address already exist!").should('exist')
+    }),
+
+    it.only("Contact Us Form",()=>{
+        //Click on 'Contact Us' button
+        cy.get("a[href='/contact_us']").click()
+
+        // Verify 'GET IN TOUCH' is visible
+        cy.findByText('Get In Touch').should('exist')
+
+        //cy.get('.contact-form').contains('Get In Touch')
+
+        // Enter name, email, subject and message
+        cy.findByPlaceholderText("Name").type("crest user")
+        cy.findByPlaceholderText("Email").type("crestuser@yopmail.com")
+        cy.findByPlaceholderText("Subject").type("Test subject")
+        cy.findByPlaceholderText("Your Message Here").type("Test Message")
+
+        // Upload file
+       
+        // Click 'Submit' button
+   
+        // Click OK button
+   
+        // Verify success message 'Success! Your details have been submitted successfull.' is visible
+   
+        // Click 'Home' button and verify that landed to home page successfully
+   
     })
 
 })
-
-
